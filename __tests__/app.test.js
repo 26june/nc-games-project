07a6d376392bc26a6c4790038f47b1fbef5ea79f
26june <gj.abordo1@gmail.com>
2022-11-14 12:08:29 +0000

    add error handling functionality and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,17 @@ const data = require("../db/data/index.js");
 beforeEach(() => seed(data));
 afterAll(() => db.end());
 
+describe("/not-a-valid-link", () => {
+  test("GET: 404 - INVALID LINKS SHOULD RESPOND WITH A NOT FOUND MESSAGE", () => {
+    return request(app)
+      .get("/invalidLink")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Error 404 - Not Found");
+      });
+  });
+});
+
 describe("/api/categories", () => {
   test("GET: 200 - SEND AN ARRAY OF OBJECTS WHICH HAS A SLUG AND DESCRIPTION PROPERTY", () => {
     return request(app)
--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
+//Not found 404
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Error 404 - Not Found" });
+});
+
 //Catch all
 app.use((err, req, res, next) => {
   console.log("Undandled error: ", err);
