54c301339c27f8354aa01780987430860c6e261f
26june <gj.abordo1@gmail.com>
2022-11-18 14:04:56 +0000

    add getUsersByUsername functionality

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,7 +1,12 @@
-const { getUsers } = require("../controllers/app.controller");
+const {
+  getUsers,
+  getUsersByUsernames,
+} = require("../controllers/app.controller");
 
 const userRouter = require("express").Router();
 
 userRouter.route("/").get(getUsers);
+
+userRouter.route("/:username").get(getUsersByUsernames);
 
 module.exports = userRouter;
